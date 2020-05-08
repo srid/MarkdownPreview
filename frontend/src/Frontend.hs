7dcfe55a6b8db94bc94f8b37608a3f8b9f0897ef
Sridhar Ratnakumar <srid@srid.ca>
2020-05-08 10:54:06 -0400

    Add function to parse markdown

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -20,6 +20,12 @@ import Reflex.Dom.Core
 import Common.Api
 import Common.Route
 
+import qualified Commonmark as CM
+
+renderMarkdown :: T.Text -> Either CM.ParseError (CM.Html ())
+renderMarkdown =
+  CM.commonmark "markdown"
+
 
 -- This runs in a monad that can be run on the client or the server.
 -- To run code in a pure client or pure server context, use one of the
